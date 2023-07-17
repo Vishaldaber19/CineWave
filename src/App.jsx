93be1ca1dd8a35a2fdb95de2f0922c75435b2e57
vishaldaber19 <sleeplesswizard019@gmail.com>
2023-07-17 23:33:56 +0530

    bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
 import SearchResult from "./pages/searchResult/SearchResult";
 import Explore from "./pages/explore/Explore";
-import PageNotFound from "./pages/Error404/PageNotFound";
+import PageNotFound from "./pages/error404/PageNotFound";
 
 function App() {
   const dispatch = useDispatch();
